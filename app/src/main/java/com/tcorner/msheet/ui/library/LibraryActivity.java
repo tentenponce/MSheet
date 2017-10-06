deddd11d3631b1a2df4da732d2b94095a6f5b2a5
Tenten Ponce <poncetenten10@gmail.com>
2017-10-06 12:43:06 +0800

    Updated Library Layout

--- app/src/main/java/com/tcorner/msheet/ui/library/LibraryActivity.java
+++ app/src/main/java/com/tcorner/msheet/ui/library/LibraryActivity.java
@@ -2,17 +2,25 @@ package com.tcorner.msheet.ui.library;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 
+import com.tcorner.msheet.R;
 import com.tcorner.msheet.ui.base.BaseActivity;
 
 import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * displays all of your music sheeeeeets
  * Created by Exequiel Egbert V. Ponce on 9/14/2017.
  */
 
-public class LibraryActivity extends BaseActivity {
+public class LibraryActivity extends BaseActivity implements LibraryMvpView {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Inject
     LibraryPresenter libraryPresenter;
@@ -20,10 +28,37 @@ public class LibraryActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library);
+        ButterKnife.bind(this);
+
+        activityComponent().inject(this);
+        libraryPresenter.attachView(this);
+
+        initViews();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        libraryPresenter.detachView();
     }
 
     @Override
     public void showError() {
+        /**/
+    }
 
+    @Override
+    public void showMusicSheets() {
+
+    }
+
+    private void initViews() {
+        /* init toolbar */
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(R.string.title_library);
+        }
     }
 }
